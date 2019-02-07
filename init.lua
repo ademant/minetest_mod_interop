bf42dcc8c7149e28c8fd8798147c502fe1a122fa
A. Demant <ademant@ademant.de>
2019-02-07 05:35:06 +0100

    remove glooptest ruby/sapphire

--- init.lua
+++ init.lua
@@ -4,10 +4,18 @@
 minetest.log("action", "[MOD]"..minetest.get_current_modname().." -- start loading from "..minetest.get_modpath(minetest.get_current_modname()))
 -- Load files
 
+-- remove glooptest ruby, if technic ruby is defined
 if glooptest ~= nil and minetest.registered_items["technic_aluminum:ruby"] ~= nil then
+	if minetest.registered_items["glooptest:mineral_ruby"] ~= nil then
+		minetest.unregister_item("glooptest:mineral_ruby")
+	end
 end
 
+-- remove glooptest sapphire, if technic sapphire is defined
 if glooptest ~= nil and minetest.registered_items["technic_aluminum:sapphire"] ~= nil then
+	if minetest.registered_items["glooptest:mineral_sapphire"] ~= nil then
+		minetest.unregister_item("glooptest:mineral_sapphire")
+	end
 end
 
 minetest.log("action", "[MOD]"..minetest.get_current_modname().." -- loaded ")
