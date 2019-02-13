ed289ec85d79ab5c2ae99dc0f390311a3d64fdec
A. Demant <ademant@ademant.de>
2019-02-13 15:49:23 +0100

    add bags to treasures

--- init.lua
+++ init.lua
@@ -18,4 +18,23 @@ if glooptest ~= nil and minetest.registered_items["technic_aluminum:sapphire"] ~
 	end
 end
 
+-- add bags to treasures
+if treasures ~= nil and minetest.registered_items["bags:small"] ~= nil then
+	if minetest.registered_items["bags:small"] ~= nil then
+		treasures.add_drop("treasures:treasure1",{items="bags:small",rarety=15})
+		treasures.add_drop("treasures:treasure2",{items="bags:small",rarety=10})
+	end
+end
+if treasures ~= nil and minetest.registered_items["bags:medium"] ~= nil then
+	if minetest.registered_items["bags:medium"] ~= nil then
+		treasures.add_drop("treasures:treasure3",{items="bags:medium",rarety=15})
+		treasures.add_drop("treasures:treasure4",{items="bags:medium",rarety=15})
+	end
+end
+if treasures ~= nil and minetest.registered_items["bags:big"] ~= nil then
+	if minetest.registered_items["bags:big"] ~= nil then
+		treasures.add_drop("treasures:treasure5",{items="bags:big",rarety=15})
+	end
+end
+
 minetest.log("action", "[MOD]"..minetest.get_current_modname().." -- loaded ")
